f1aac0795a3371f5b8cca4d58fa3473427cd00f4
sai-cs-stud <srm275@scarletmail.rutgers.edu>
2021-04-01 14:51:09 -0400

    client py update

--- client.py
+++ client.py
@@ -7,16 +7,16 @@ import socket
 #from ts import top_server
 import sys
 
-rsHostname = sys.argv[1]
-rsListenPort = int(sys.argv[2])
-tsListenPort = int(sys.argv[3])
+lsHostname = sys.argv[1]
+lsListenPort = int(sys.argv[2])
+#tsListenPort = int(sys.argv[3])
 
 
 def client():
     try:
-        cli_rss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        cli_lss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("[C]: Client RS socket created")
-        cli_tss = None
+        #cli_tss = None
     except socket.error as err:
         print('socket open error: {} \n'.format(err))
         exit()
@@ -24,17 +24,17 @@ def client():
     # List for request data
     hostreqs = []
     # Read in data from txt file
-    with open("PROJI-HNS.txt") as txtdata:
+    with open("PROJ2-HNS.txt") as txtdata:
         for line in txtdata:
             hostreqs.append(line)
     
     # Define port for rs socket
-    port1 = rsListenPort
+    port1 = lsListenPort
     #localhost_addr1 = socket.gethostbyname(socket.gethostname())
 
     # connect to servers on local(for now)
-    server_binding1 = (rsHostname,port1)
-    cli_rss.connect(server_binding1)
+    server_binding1 = (lsHostname,port1)
+    cli_lss.connect(server_binding1)
     # Open resolved and clear
     f = open("RESOLVED.txt", "r+")
     f.seek(0)
@@ -43,58 +43,20 @@ def client():
     while True:
         # Send and recieve here
         for req in hostreqs:
-            cli_rss.send(req.encode('UTF-8'))
+            cli_lss.send(req.encode('UTF-8'))
             #print("[C]: Requesting " + req)
-            data_from_rs = "{}".format(cli_rss.recv(200).decode('UTF-8'))
-            print("[C]: Recieved\n" + data_from_rs)
-            dataline = data_from_rs.split()
-            if dataline[2] == 'NS':
-                try:
-                    if cli_tss is None:
-                        cli_tss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        print("[C]: Client TS socket created")
-                         # Define port for ts socket
-                        port2 = tsListenPort
-                        localhost_addr2 = socket.gethostbyname(socket.gethostname())
-
-                        # connect to ts on local(for now)
-                        server_binding2 = (rsHostname, port2)
-                        cli_tss.connect(server_binding2)
-                except socket.error as err:
-                    print('socket open error: {} \n'.format(err))
-                    exit()
-                cli_tss.send(req.encode('UTF-8'))
-                #print("[C]: Requesting " + req)
-                data_from_ts = "{}".format(cli_tss.recv(200).decode('UTF-8'))
-                print("[C]: Recieved\n" + data_from_ts)
-                f.write(data_from_ts+"\n")
-            else:
-                f.write(data_from_rs+"\n")
+            data_from_ls = "{}".format(cli_lss.recv(200).decode('UTF-8'))
+            print("[C]: Recieved\n" + data_from_ls)
+            dataline = data_from_ls.split()
+            f.write(data_from_ls+"\n")
         break
 
     f.close()
     # Close client sockets
-    cli_tss.send('disconnect'.encode('UTF-8'))
-    cli_tss.close()
-
-    cli_rss.send('disconnect'.encode('UTF-8'))
-    cli_rss.close()
+    cli_lss.send('disconnect'.encode('UTF-8'))
+    cli_lss.close()
     exit()
 
 if __name__ == "__main__":
     client()
-#    t1 = threading.Thread(name='rs', target=root_server)
-#    t1.start()
-
-#    time.sleep(random.random() * 5)
-#    t3 = threading.Thread(name='ts', target=top_server)
-#    t3.start()
-
-#    time.sleep(random.random() * 5)
-#    t2 = threading.Thread(name='client', target=client)
-#    t2.start()
-
-    
-
-#    time.sleep(30)
     print("Done.")
